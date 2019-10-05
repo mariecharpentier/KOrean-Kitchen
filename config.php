e3b89ae233dc7d50ecb4c6571d08ed45419664be
mariecharpentier <44870518+mariecharpentier@users.noreply.github.com>
2019-10-05 16:05:53 +0200

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 //config.php
 $pdo = new PDO('mysql:host=localhost;dbname=kkitchen', 'root', '');
-// $pdo = new PDO('mysql:host=jessicastymarie.mysql.db;dbname=jessicastymarie', 'jessicastymarie', 'Australia2016');
+
 
 // Paramétrage de la liaison PHP <-> MySQL, les données sont encodées en UTF-8.
 $pdo->exec('SET NAMES UTF8');
