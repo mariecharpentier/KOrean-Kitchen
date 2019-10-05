ead7745321f79a98c6c95211f19ec042adf28de8
mariecharpentier <44870518+mariecharpentier@users.noreply.github.com>
2019-10-05 16:07:27 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  setTimeout(function(){
+    $('.header-meal').fadeIn('slow');
+  }, 1000);
+
+
+});
---
+++ js/relocation.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Redirection to home page
+window.setTimeout("location=('http://jessicas-ko-kitchen.com/index.php');",1200);
