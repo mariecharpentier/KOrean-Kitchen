ed88ff106d8e8f1cca77923e07e7209f56aacd74
mariecharpentier <44870518+mariecharpentier@users.noreply.github.com>
2019-12-05 17:26:52 +0100

    Add files via upload

---
+++ favorites.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+include 'classes/Database.class.php';
+include 'classes/User.class.php';
+include 'config.php';
+
+$template = 'favorites';
+include 'layout.phtml';
+
+ ?>
